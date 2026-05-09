58b09baea9cbb92a03ddec2f8e99ab124d695561
buto0071 <mk@hitprotocol.org>
2026-05-09 15:57:58 +0200

    Clean up patch files

--- patch2.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/cellManager.ts', 'utf8');
-
-content = content.replace(/lastCell\.model\.value/g, "(lastCell.model as any).value");
-fs.writeFileSync('src/cellManager.ts', content);
-console.log('Patched types');
